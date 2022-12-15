c2a93295c2d6382c452143280235b3dc0a7394c5
Yahia Abbas <91669499+YahiaSameh@users.noreply.github.com>
2022-12-15 12:53:02 +0200

    Navbar and Title logo created

---
+++ frontend/src/EssentialComponents/TopNav.js
@@ -0,0 +1,39 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import bootstartp from 'bootstrap/dist/css/bootstrap.min.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './topnav.css';
+import logo from '../logo.png'; 
+function TopNav() {
+  return (
+    <Navbar className='color-nav' bg="E14D2A !important" expand="lg">
+      <Container>
+      <img className='logo'src={logo} />
+        <Navbar.Brand href="#home">Ticketaty</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Browse</Nav.Link>
+
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopNav;
