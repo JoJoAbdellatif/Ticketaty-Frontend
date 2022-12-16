7fc38b81b2c004fb2735dcdb846e5e20d905c625
Yahia Abbas <91669499+YahiaSameh@users.noreply.github.com>
2022-12-16 23:08:34 +0200

    Card V1.0

--- frontend/src/InfoCards/MatchCards.js
+++ frontend/src/InfoCards/MatchCards.js
@@ -3,18 +3,23 @@ import "./styles.scss";
 import { useState } from "react";
 import cn from "classnames";
 import Button from 'react-bootstrap/Button';
+import { alignPropType } from "react-bootstrap/esm/types";
 
 function FlipCard({card}) {
    
 
   const [showBack, setShowBack] = useState(false); 
-
+  const date ="   "+ card.dateUtc.split("T")[0];
+  const time = card.dateUtc.split("T")[1];
   function handleClick() {  
       setShowBack(!showBack); 
 
   } 
 
   return (
+    <div className="container1">
+
+    
     <div
       className="flip-card-outer"
       onClick={handleClick} 
@@ -27,24 +32,42 @@ function FlipCard({card}) {
       >
         <div className="card front">    
         <div className="card-body">
+        <img className='Homeimage'src={card.homeimage} ></img>
                 <h5 className="title">{card.homeTeam} vs {card.awayTeam}</h5>
+                <img className='Awayimage'src={card.awayimage} ></img>
+            <div></div>
             </div>
+       
         </div>
         <div className="card back">
-        <h5 className="card-title">{card.matchNumber}</h5>
+          <div          className="Match">
+        <h5 className="card-title">Match Number:  {card.matchNumber}</h5>
+        </div>
+        <div className="Round">
+        <h5 className="card-title">Round Number:  {card.roundNumber}</h5>
+        </div>
+        <div className="Group">
+        <h5 className="card-title">Group:  {card.group}</h5>
+        </div>
+        <div className="Dateloc">
+        <h5 className="card-title"> FIFA World Cup 2022™{date}</h5>
+        {/* <h5 className="card-title"> {time}</h5> */}
+
+     
+        </div >
+        <div className="card-body d-flex justify-content-right align-items-center" >
+        {/* <h5 className="card-title">  {card.location}</h5> */}
+        </div>
+        <div >
+        <Button className='booknowbutton'variant="primary"  >
       
-        <h5 className="card-title">    {card.roundNumber}</h5>
-        <h5 className="card-title">
-           {card.dateUtc}</h5>
-        <h5 className="card-title">
-           {card.location}</h5>
-        <h5 className="card-title">
-           {card.group}</h5>
-        <div>
-      <Button variant="primary">Primary</Button>
+      Book Now!</Button>
       </div>
         </div>
       </div>
+   
+    </div>
+   
     </div>
   );
 }
--- frontend/src/InfoCards/ShowCards.js
+++ frontend/src/InfoCards/ShowCards.js
@@ -4,6 +4,64 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.scss";
 
 const cards = [
+ {
+  "_id": "63976f75d4aadd7d875e8ec9",
+  "matchNumber": 1,
+  "roundNumber": 1,
+  "dateUtc": "2022-11-20T16:00:00Z",
+  "location": "Al Bayt Stadium",
+  "availability": {
+      "category1": {
+          "count": 19,
+          "price": 75
+      },
+      "category2": {
+          "count": 18,
+          "price": 125
+      },
+      "category3": {
+          "count": 20,
+          "price": 195
+      }
+  },
+  "homeTeam": "Qatar",
+  "awayTeam": "Ecuador",
+  "homeimage":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flag_of_Qatar_%283-2%29.svg/1200px-Flag_of_Qatar_%283-2%29.svg.png",
+    "awayimage":"https://media.istockphoto.com/id/689094754/vector/flag-of-ecuador.jpg?s=612x612&w=0&k=20&c=up-thIaaQ5h_XtoCXOwUJHu3PsMnPOcKR0Vv-jUhf1c=",
+  "group": "A",
+  "category1": {
+      "count": 19
+  }
+},
+{
+    "_id": "63976f75d4aadd7d875e8ec9",
+    "matchNumber": 1,
+    "roundNumber": 1,
+    "dateUtc": "2022-11-20T16:00:00Z",
+    "location": "Al Bayt Stadium",
+    "availability": {
+        "category1": {
+            "count": 19,
+            "price": 75
+        },
+        "category2": {
+            "count": 18,
+            "price": 125
+        },
+        "category3": {
+            "count": 20,
+            "price": 195
+        }
+    },
+    "homeTeam": "Qatar",
+    "awayTeam": "Ecuador",
+    "homeimage":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flag_of_Qatar_%283-2%29.svg/1200px-Flag_of_Qatar_%283-2%29.svg.png",
+      "awayimage":"https://media.istockphoto.com/id/689094754/vector/flag-of-ecuador.jpg?s=612x612&w=0&k=20&c=up-thIaaQ5h_XtoCXOwUJHu3PsMnPOcKR0Vv-jUhf1c=",
+    "group": "A",
+    "category1": {
+        "count": 19
+    }
+  },
   {
     "_id": "63976f75d4aadd7d875e8ec9",
     "matchNumber": 1,
@@ -26,118 +84,43 @@ const cards = [
     },
     "homeTeam": "Qatar",
     "awayTeam": "Ecuador",
+    "homeimage":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flag_of_Qatar_%283-2%29.svg/1200px-Flag_of_Qatar_%283-2%29.svg.png",
+      "awayimage":"https://media.istockphoto.com/id/689094754/vector/flag-of-ecuador.jpg?s=612x612&w=0&k=20&c=up-thIaaQ5h_XtoCXOwUJHu3PsMnPOcKR0Vv-jUhf1c=",
     "group": "A",
     "category1": {
         "count": 19
     }
-},
-{
-  "_id": "63976f75d4aadd7d875e8ec9",
-  "matchNumber": 1,
-  "roundNumber": 1,
-  "dateUtc": "2022-11-20T16:00:00Z",
-  "location": "Al Bayt Stadium",
-  "availability": {
-      "category1": {
-          "count": 19,
-          "price": 75
-      },
-      "category2": {
-          "count": 18,
-          "price": 125
-      },
-      "category3": {
-          "count": 20,
-          "price": 195
-      }
   },
-  "homeTeam": "Qatar",
-  "awayTeam": "Ecuador",
-  "group": "A",
-  "category1": {
-      "count": 19
+  {
+    "_id": "63976f75d4aadd7d875e8ec9",
+    "matchNumber": 1,
+    "roundNumber": 1,
+    "dateUtc": "2022-11-20T16:00:00Z",
+    "location": "Al Bayt Stadium",
+    "availability": {
+        "category1": {
+            "count": 19,
+            "price": 75
+        },
+        "category2": {
+            "count": 18,
+            "price": 125
+        },
+        "category3": {
+            "count": 20,
+            "price": 195
+        }
+    },
+    "homeTeam": "Qatar",
+    "awayTeam": "Ecuador",
+    "homeimage":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flag_of_Qatar_%283-2%29.svg/1200px-Flag_of_Qatar_%283-2%29.svg.png",
+      "awayimage":"https://media.istockphoto.com/id/689094754/vector/flag-of-ecuador.jpg?s=612x612&w=0&k=20&c=up-thIaaQ5h_XtoCXOwUJHu3PsMnPOcKR0Vv-jUhf1c=",
+    "group": "A",
+    "category1": {
+        "count": 19
+    }
   }
-},{
-  "_id": "63976f75d4aadd7d875e8ec9",
-  "matchNumber": 1,
-  "roundNumber": 1,
-  "dateUtc": "2022-11-20T16:00:00Z",
-  "location": "Al Bayt Stadium",
-  "availability": {
-      "category1": {
-          "count": 19,
-          "price": 75
-      },
-      "category2": {
-          "count": 18,
-          "price": 125
-      },
-      "category3": {
-          "count": 20,
-          "price": 195
-      }
-  },
-  "homeTeam": "Qatar",
-  "awayTeam": "Ecuador",
-  "group": "A",
-  "category1": {
-      "count": 19
-  }
-},
-{
-  "_id": "63976f75d4aadd7d875e8ec9",
-  "matchNumber": 1,
-  "roundNumber": 1,
-  "dateUtc": "2022-11-20T16:00:00Z",
-  "location": "Al Bayt Stadium",
-  "availability": {
-      "category1": {
-          "count": 19,
-          "price": 75
-      },
-      "category2": {
-          "count": 18,
-          "price": 125
-      },
-      "category3": {
-          "count": 20,
-          "price": 195
-      }
-  },
-  "homeTeam": "Qatar",
-  "awayTeam": "Ecuador",
-  "group": "A",
-  "category1": {
-      "count": 19
-  }
-},
-{
-  "_id": "63976f75d4aadd7d875e8ec9",
-  "matchNumber": 1,
-  "roundNumber": 1,
-  "dateUtc": "2022-11-20T16:00:00Z",
-  "location": "Al Bayt Stadium",
-  "availability": {
-      "category1": {
-          "count": 19,
-          "price": 75
-      },
-      "category2": {
-          "count": 18,
-          "price": 125
-      },
-      "category3": {
-          "count": 20,
-          "price": 195
-      }
-  },
-  "homeTeam": "Qatar",
-  "awayTeam": "Ecuador",
-  "group": "A",
-  "category1": {
-      "count": 19
-  }
-},
+
 
 ]
 
